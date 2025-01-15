88be2e8092b16fab03a7267e2ba01930bf52ace5
Yaroslav Zghoba <yaroslavzghoba@gmail.com>
2025-01-15 17:17:46 +0100

    Added utils for cookie management to simplify unit tests

--- src/test/kotlin/com/yaroslavzghoba/AuthenticationTest.kt
+++ src/test/kotlin/com/yaroslavzghoba/AuthenticationTest.kt
@@ -2,14 +2,13 @@ package com.yaroslavzghoba
 
 import com.yaroslavzghoba.model.InputCredentials
 import com.yaroslavzghoba.model.TokenRegistrationResponse
+import com.yaroslavzghoba.utils.rawCookie
 import io.ktor.client.call.*
 import io.ktor.client.request.*
 import io.ktor.http.*
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
-private const val COOKIE_REQUEST_PARAM_NAME = "Cookie"
-private const val COOKIE_RESPONSE_PARAM_NAME = "Set-Cookie"
 private const val STRONG_TOKEN =
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiIvYXBpIiwiaXNzIjoiL2FwaS9yZWdpc3RlciIsInN0cm9uZyI6InRydWUiLCJpYXQiOjE3MjY4NTIzOTd9.WW2fj_gRrGD2I6BklSHIS03Q8hBUMUhHxX7jDIcKs-s"
 private const val NOT_STRONG_TOKEN =
@@ -253,11 +252,11 @@ class AuthenticationTest {
                 bearerAuth(NOT_STRONG_TOKEN)
                 setBody(inputCredentials)
             }
-            val cookies = response0.headers[COOKIE_RESPONSE_PARAM_NAME]  // Contains the user's session
+            val rawCookie = response0.rawCookie()  // Contains the user's session id
 
             // Get access to the session-protected resource
             val response1 = client.get("/v1/account") {
-                header(key = COOKIE_REQUEST_PARAM_NAME, value = cookies)
+                rawCookie(value = rawCookie)
                 bearerAuth(NOT_STRONG_TOKEN)
             }
 
@@ -285,17 +284,17 @@ class AuthenticationTest {
                 bearerAuth(NOT_STRONG_TOKEN)
                 setBody(inputCredentials)
             }
-            val cookies = response0.headers[COOKIE_RESPONSE_PARAM_NAME]  // Contains the user's session
+            val rawCookie = response0.rawCookie()  // Contains the user's session
 
             // Close the session on the server's side
             client.post("/v1/account/logout") {
-                header(key = COOKIE_REQUEST_PARAM_NAME, value = cookies)
+                rawCookie(value = rawCookie)
                 bearerAuth(NOT_STRONG_TOKEN)
             }
 
             // Try to get access to the session-protected resource after logout
             val response1 = client.get("/v1/account") {
-                header(key = COOKIE_REQUEST_PARAM_NAME, value = cookies)
+                rawCookie(value = rawCookie)
                 bearerAuth(NOT_STRONG_TOKEN)
             }
 
@@ -317,17 +316,17 @@ class AuthenticationTest {
                 setBody(inputCredentials)
             }
 
-            // Login the registed user and extract its cookie
+            // Login the user and extract its cookie
             val response0 = client.post("/v1/account/login") {
                 contentType(ContentType.Application.Json)
                 bearerAuth(NOT_STRONG_TOKEN)
                 setBody(inputCredentials)
             }
-            val cookies = response0.headers[COOKIE_RESPONSE_PARAM_NAME]  // Contains the user's session
+            val cookies = response0.rawCookie()  // Contains the user's session
 
             // Try to register a new non-strong access token
             val response1 = client.post("/register") {
-                header(key = COOKIE_REQUEST_PARAM_NAME, value = cookies)
+                rawCookie(value = cookies)
             }
 
             assertEquals(
